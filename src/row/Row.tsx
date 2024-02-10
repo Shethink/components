4e0dbc14fe86557551eb0c493261ee3482bf15db
debugger-anubhav <anubhav.kumar98@gmail.com>
2024-02-10 19:50:38 +0530

    version bump to test row changes

--- src/row/Row.tsx
+++ src/row/Row.tsx
@@ -4,12 +4,13 @@ import { CSSProperties } from "@mui/styles";
 const Row = ({
   children,
   style,
+  ...props
 }: {
   children: React.ReactNode;
   style?: CSSProperties;
 }) => {
   return (
-    <div style={{ display: "flex", alignItems: "center", ...style }}>
+    <div style={{ display: "flex", alignItems: "center", ...style }} {...props}>
       {children}
     </div>
   );
