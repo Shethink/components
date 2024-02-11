7291e7b50cc1ad03ce735a10b0c06b31f07a6680
raman <raman.risaanva@2wls.net>
2024-02-11 12:35:57 +0530

    Changes Related to Checkbox and custom select

--- src/custom-select/CustomSelect.tsx
+++ src/custom-select/CustomSelect.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import { Container, Icon, TextField, } from '@mui/material';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { BoxContainer, OptionListBox, DropDownOption, EmptyRadio, CheckCircleIconCustom, OptionItemBox, KeyboardArrowUpIconCustom } from './styles';
-import Typography from '../typography';
-import Divider from '../divider';
-import { Chip } from '@mui/material';
-
+import React, { useState } from "react";
+import { Container, Icon, TextField } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {
+    BoxContainer,
+    OptionListBox,
+    DropDownOption,
+    EmptyRadio,
+    CheckCircleIconCustom,
+    OptionItemBox,
+    KeyboardArrowUpIconCustom,
+    Text
+} from "./styles";
+import Typography from "../typography";
+import Divider from "../divider";
+import { Chip } from "@mui/material";
+import SearchIcon from "src/icon/search-icon";
 
 interface Item {
     id: number;
@@ -15,7 +24,7 @@ interface Item {
 interface Props {
     option: Item[];
     selectedlist: Item[];
-    setselectedList: React.Dispatch<React.SetStateAction<Item[]>>
+    setselectedList: React.Dispatch<React.SetStateAction<Item[]>>;
 }
 
 const CustomSelect = ({ option, selectedlist, setselectedList }: Props) => {
@@ -23,53 +32,86 @@ const CustomSelect = ({ option, selectedlist, setselectedList }: Props) => {
 
     const handleOnClick = (id: number, name: string) => {
         const array = [...selectedlist];
-        array.push({ id: id, name: name })
-        setselectedList(array)
-    }
-    const handleRemoveItem = (id: number, name: string) => {
-        const array = selectedlist?.filter((item) => item.id !== id)
+        array.push({ id: id, name: name });
         setselectedList(array);
-    }
+    };
+    const handleRemoveItem = (id: number, name: string) => {
+        const array = selectedlist?.filter((item) => item.id !== id);
+        setselectedList(array);
+    };
 
     const handleChips = () => {
+
         if (isOpen) {
-            return "Services"
+            return <Container style={{ display: 'flex', justifyContent: 'center', alignItems: "center", padding: 0 }}>
+                <SearchIcon style={{ marginRight: "10px", padding: "0", display: 'flex', justifyContent: 'center', alignItems: "center", }} height={24} width={20} /> <Text fontSize={"20px"} fontWeight={"500"} fontFamily={"sans-serif"} color={"#192023"}>Services</Text></Container>;
         } else {
-            return selectedlist?.map((item: { id: number, name: string }) => {
-                return <Chip style={{ padding: "10px", margin: "2px" }} label={item?.name} onDelete={() => handleRemoveItem(item?.id, item?.name)} />
-            })
+            if (selectedlist.length == 0) {
+                return <Container style={{ display: 'flex', justifyContent: 'center', alignItems: "center", padding: 0 }}>
+                    <SearchIcon style={{ marginRight: "10px", padding: "0", display: 'flex', justifyContent: 'center', alignItems: "center", }} height={24} width={20} /> <Text fontSize={"20px"} fontWeight={"500"} fontFamily={"sans-serif"} color={"#192023"}>Services</Text></Container>;
+            } else
+                return selectedlist?.map((item: { id: number; name: string }) => {
+                    return (
+                        <Chip
+                            style={{ padding: "10px", margin: "2px" }}
+                            label={item?.name}
+                            onDelete={() => handleRemoveItem(item?.id, item?.name)}
+                        />
+                    );
+                });
         }
-    }
+    };
 
     return (
         <Container>
-            <Typography>{"Select"}</Typography>
+            <Text fontSize={"20px"} fontWeight={"500"} fontFamily={"sans-serif"} color={"#192023"}>Services</Text>
             <BoxContainer onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
                 <Typography>{handleChips()}</Typography>
                 <KeyboardArrowUpIconCustom isOpen={isOpen}></KeyboardArrowUpIconCustom>
             </BoxContainer>
-            {isOpen &&
-                <DropDownOption >
-                    {selectedlist?.length > 0 && <OptionListBox>
-                        {selectedlist?.map((item: { id: number, name: string }) => {
-                            return <OptionItemBox onClick={() => handleRemoveItem(item?.id, item?.name)} key={item.id}>
-                                <CheckCircleIconCustom /> <Typography>{item.name}</Typography>
-                            </OptionItemBox>
-                        })}
-                    </OptionListBox>}
-                    {selectedlist?.length > 0 &&
-                        <Container style={{ width: '99%' }}><Divider color='#D8D7E1' height='100%' width='100%' /></Container>}
+            {isOpen && (
+                <DropDownOption>
+                    {selectedlist?.length > 0 && (
+                        <OptionListBox>
+                            {selectedlist?.map((item: { id: number; name: string }) => {
+                                return (
+                                    <OptionItemBox
+                                        onClick={() => handleRemoveItem(item?.id, item?.name)}
+                                        key={item.id}
+                                    >
+                                        <CheckCircleIconCustom />{" "}
+                                        <Text fontSize={"20px"} fontWeight={"500"} fontFamily={"sans-serif"} color={"#192023"} >{item.name}</Text>
+                                    </OptionItemBox>
+                                );
+                            })}
+                        </OptionListBox>
+                    )}
+                    {selectedlist?.length > 0 && (
+                        <Container style={{ width: "99%" }}>
+                            <Divider color="#D8D7E1" height="100%" width="100%" />
+                        </Container>
+                    )}
                     <OptionListBox>
-                        {option?.filter(option => !selectedlist.some(item => item.id === option.id))?.map((item) => {
-                            return <OptionItemBox onClick={() => handleOnClick(item?.id, item?.name)} key={item.id}>
-                                <EmptyRadio></EmptyRadio> <Typography>{item.name}</Typography>
-                            </OptionItemBox>
-                        })}
+                        {option
+                            ?.filter(
+                                (option) => !selectedlist.some((item) => item.id === option.id)
+                            )
+                            ?.map((item) => {
+                                return (
+                                    <OptionItemBox
+                                        onClick={() => handleOnClick(item?.id, item?.name)}
+                                        key={item.id}>
+                                        <EmptyRadio></EmptyRadio>{" "}
+                                        <Text fontSize={"20px"} fontWeight={"500"} fontFamily={"sans-serif"} color={"#192023"} >{item.name}</Text>
+                                    </OptionItemBox>
+                                );
+                            })}
                     </OptionListBox>
-                </DropDownOption>}
+                </DropDownOption>
+            )
+            }
         </Container >
-    )
-}
+    );
+};
 
-
-export default CustomSelect
+export default CustomSelect;
--- src/custom-select/styles.ts
+++ src/custom-select/styles.ts
@@ -1,12 +1,14 @@
 import { styled } from "@mui/material/styles";
 import Container from "../container";
+import { Container as AnotherContainer } from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Typography from "src/typography";
 
 export const BoxContainer = styled(Container)<{ isOpen: boolean }>(({ isOpen, theme }) => ({
     border: `1px solid #D8D7E1`,
     borderRadius: isOpen ? "6px 6px 0px 0px" : "6px 6px 6px 6px",
-    padding: "10px",
+    padding: "5px",
     display: 'flex',
     justifyContent: "space-between",
     alignItems: "center"
@@ -38,21 +40,21 @@ export const EmptyRadio = styled(Container)(() => ({
     margin: "0 5px 0 0",
     border: "1px solid #D7D8E1",
     borderRadius: "50%",
-    width: "16px",
-    height: "16px",
+    width: "20px",
+    height: "20px",
     maxWidth: "fit-Content"
 }))
 
 export const CheckCircleIconCustom = styled(CheckCircleIcon)(() => ({
     padding: "0",
     margin: "0 5px 0 0",
-    width: "18px",
-    height: "18px",
+    width: "22px",
+    height: "22px",
     color: "#02BC7D"
 }))
 
 
-export const OptionItemBox = styled(Container)(() => ({
+export const OptionItemBox = styled(AnotherContainer)(() => ({
     padding: "5px 10px !important",
     margin: "0",
     display: 'flex',
@@ -71,6 +73,13 @@ export const KeyboardArrowUpIconCustom = styled(KeyboardArrowUpIcon)<{ isOpen: b
     color: '#393a69',
     transform: "rotate(180deg)",
     transition: "transform 0.5s ease"
+}))
+
+export const Text = styled(Typography)<{ fontSize: string | undefined, fontFamily: string | undefined, color: string | undefined, fontWeight: string | undefined }>(({ fontSize, fontFamily, fontWeight, color }) => ({
+    fontSize: fontSize ?? "16px",
+    fontFamily: fontFamily ?? 'sans-serif',
+    fontWeight: fontWeight ?? "900",
+    color: color ?? "#fff",
 }))
 
 
