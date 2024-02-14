11a7df1c95dcd05a4f9dab1cbb6225109bb9021c
ramanrisaanva <50055898+ramanrisaanva@users.noreply.github.com>
2024-02-14 12:36:50 +0530

    fix(icons): correcting imports (#8)
    Co-authored-by: raman <raman.risaanva@2wls.net>

--- src/icon/index.ts
+++ src/icon/index.ts
@@ -1,9 +1,7 @@
-import {
-  AutoAwesome,
-  Close,
-  Star,
-  ArrowBack,
-  Visibility,
-} from "@mui/icons-material";
+import AutoAwesome from "@mui/icons-material/AutoAwesome";
+import Close from '@mui/icons-material/Close'
+import Star from '@mui/icons-material/Star'
+import ArrowBack from '@mui/icons-material/ArrowBack'
+import Visibility from '@mui/icons-material/Visibility'
 
 export { AutoAwesome, Close, Star, ArrowBack, Visibility };
